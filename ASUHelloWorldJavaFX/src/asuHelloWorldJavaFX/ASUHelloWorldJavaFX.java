5d7f8dcfe9c7ae67b000f82f1ce18c3231461fbc
Paarth Batra <paarthb@icloud.com>
2024-09-24 15:56:42 -0700

    Second Commit

--- ASUHelloWorldJavaFX/src/asuHelloWorldJavaFX/ASUHelloWorldJavaFX.java
+++ ASUHelloWorldJavaFX/src/asuHelloWorldJavaFX/ASUHelloWorldJavaFX.java
@@ -7,24 +7,25 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
- 
+
 public class ASUHelloWorldJavaFX extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
-    public void start(Stage primaryStage) {
+
+    @Override
+	public void start(Stage primaryStage) {
     	System.out.println("ASU Hello World!");
     	System.out.println("It started!");
-        primaryStage.setTitle("ASU Hello World Spring 2024");
+        primaryStage.setTitle("In Class GitHub Activity");
         Button btn = new Button();
-        btn.setText("Display: 'ASU says: Hello World!'");
+        btn.setText("Neel's Second Commit");
         btn.setOnAction(new EventHandler<>() {
             public void handle(ActionEvent event) {
                 System.out.println("ASU: Hello World!");
             }
         });
-        
+
         StackPane root = new StackPane();
         root.getChildren().add(btn);
         primaryStage.setScene(new Scene(root, 300, 250));
